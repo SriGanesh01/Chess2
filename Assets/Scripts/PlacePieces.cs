c400bc61c067e40a387ccfdf41e7e37d424873b9
SriGanesh01 <psriganesh2005@gmail.com>
2024-08-08 10:08:29 +0530

    Added Invalid FEN detection

--- Assets/Scripts/PlacePieces.cs
+++ Assets/Scripts/PlacePieces.cs
@@ -4,14 +4,87 @@ using UnityEngine;
 
 public class PlacePieces : MonoBehaviour
 {
-    private string StartingFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    private string StartingFEN = "2R3B1/4K3/2pN4/1N2kppb/7p/n2p2P1/5P2/1n5Q";
 
     public Pieces Pieces;
 
     // Start is called before the first frame update
     void Start()
     {
-        PlacePiecesBasedOnFEN();
+        if (CheckIfFENIsRightLength())
+        {
+            PlacePiecesBasedOnFEN();
+        }
+        else
+        {
+            Debug.LogError("FEN is not the right length");
+        }
+    }
+
+    bool CheckIfFENIsRightLength()
+    {
+        string[] FENParts = StartingFEN.Split(' ');
+        string[] FENRows = FENParts[0].Split('/');
+        int q = 0;
+
+        for (int i = 0; i < FENRows.Length; i++)
+        {
+            q = 0;
+            for (int j = 0; j < FENRows[i].Length; j++)
+            {
+
+                char fenChar = FENRows[i][j];
+
+                switch (fenChar){
+                    case 'P':
+                    case 'R':
+                    case 'N':
+                    case 'B':
+                    case 'Q':
+                    case 'K':
+                    case 'p':
+                    case 'r':
+                    case 'n':
+                    case 'b':
+                    case 'q':
+                    case 'k':
+                        q++;
+                        break;
+                    case '1':
+                        q=q+1;
+                        break;
+                    case '2':
+                        q=q+2;
+                        break;
+                    case '3':
+                        q=q+3;
+                        break;
+                    case '4':
+                        q=q+4;
+                        break;
+                    case '5':
+                        q=q+5;
+                        break;
+                    case '6':
+                        q=q+6;
+                        break;
+                    case '7':
+                        q=q+7;
+                        break;
+                    case '8':
+                        q=q+8;
+                        break;
+                    }
+            }
+        }
+        if (q != 8)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }  
     }
 
     void PlacePiecesBasedOnFEN()
